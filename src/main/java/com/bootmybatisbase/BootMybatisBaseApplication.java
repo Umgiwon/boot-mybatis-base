2749f3a76abea60b20d4371d34e689d3a4d6fd91
umgiwon <ugwzzang@gmail.com>
2026-01-12 16:45:17 +0900

    # initial commit

---
+++ src/main/java/com/bootmybatisbase/BootMybatisBaseApplication.java
@@ -0,0 +1,13 @@
+package com.bootmybatisbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootmybatisbase/BootMybatisBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootmybatisbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMybatisBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
